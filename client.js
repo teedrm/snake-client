0da167d95ea9fb5df1d4fd59ef99eba9970db0b0
Thy Nguyen <tee@Thys-iMac.local>
2022-11-23 17:49:53 -0700

    set up

---
+++ client.js
@@ -0,0 +1,20 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "165.227.47.243",
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("data", ()=>{
+    console.log("you ded cuz you idled")
+});
+
+  return conn;
+};
+
+module.exports = { connect };
--- play.js
+++ play.js
@@ -1,21 +1,23 @@
-const net = require("net");
+const { connect } = require('./client');
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "165.227.47.243",
-    port: 50541,
-  });
+// const net = require("net");
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
+// // establishes a connection with the game server
+// const connect = function () {
+//   const conn = net.createConnection({
+//     host: "165.227.47.243",
+//     port: 50541,
+//   });
 
-  conn.on("data", ()=>{
-    console.log("you ded cuz you idled")
-});
+//   // interpret incoming data as text
+//   conn.setEncoding("utf8");
 
-  return conn;
-};
+//   conn.on("data", ()=>{
+//     console.log("you ded cuz you idled")
+// });
+
+//   return conn;
+// };
 
 console.log("Connecting ...");
 connect();
