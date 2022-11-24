802676ea49592381b7380844cf6ef04cd62e0bf7
Thy Nguyen <tee@Thys-iMac.local>
2022-11-24 00:47:25 -0700

    set up

---
+++ input.js
@@ -0,0 +1,32 @@
+const setupInput = function (conn) {
+    connection = conn;
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding("utf8");
+    stdin.resume();
+    stdin.on("data", handleUserInput);
+    return stdin;
+};
+
+const handleUserInput = function (key) {
+    // \u0003 maps to ctrl+c input
+    if (key === '\u0003') {
+        process.exit();
+    } else if (key === 'w') {
+        conn.write('Move: up');
+    } else if (key === 'd') {
+        conn.write('Move: right');
+    } else if (key === 's') {
+        conn.write('Move: down');
+    } else if (key === 'a') {
+        conn.write('Move: left')
+    } else if (key === 'q') {
+        conn.write('Hi');
+    } else if (key === 'w') {
+        conn.write('Hello');
+    } else if (key === 'e') {
+        conn.write('Bye');
+    }
+};
+
+module.exports = { setupInput };
--- play.js
+++ play.js
@@ -1,57 +1,7 @@
-const { connect } = require('./client');
-
-// const net = require("net");
-
-// // establishes a connection with the game server
-// const connect = function () {
-//   const conn = net.createConnection({
-//     host: "165.227.47.243",
-//     port: 50541,
-//   });
-
-//   // interpret incoming data as text
-//   conn.setEncoding("utf8");
-
-//   conn.on("data", ()=>{
-//     console.log("you ded cuz you idled")
-// });
-
-//   return conn;
-// };
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
 connect();
 
-const setupInput = function (conn) {
-    connection = conn;
-    const stdin = process.stdin;
-    stdin.setRawMode(true);
-    stdin.setEncoding("utf8");
-    stdin.resume();
-    stdin.on("data", handleUserInput);
-    return stdin;
-};
-
-const handleUserInput = function (key) {
-    // \u0003 maps to ctrl+c input
-    if (key === '\u0003') {
-        process.exit();
-    } else if (key === 'w') {
-        conn.write('Move: up');
-    } else if (key === 'd') {
-        conn.write('Move: right');
-    } else if (key === 's') {
-        conn.write('Move: down');
-    } else if (key === 'a') {
-        conn.write('Move: left')
-    } else if (key === 'q') {
-        conn.write('Hi');
-    } else if (key === 'w') {
-        conn.write('Hello');
-    } else if (key === 'e') {
-        conn.write('Bye');
-    }
-};
-
-
-
+setupInput();
